3dee0d8f94315c02ab557f0634f8d81eb3faaae8
dannypete <dannypete@users.noreply.github.com>
2023-01-12 15:35:40 -0700

    removed delete_hosts' --full-match since it didnt make sense with too many --remove-by's. updated its main candidate, cpe, to partial-cpe which seems more likely how someone would use it anyway. if someone wanted a more exact match, they would (should) just provide a more complete cpe string lol

--- plugins/delete_hosts.py
+++ plugins/delete_hosts.py
@@ -15,7 +15,7 @@ class FilterParameters(Enum):
     ip = "host-ip"
     fqdn = "host-fqdn"
     was_credential_scanned = "Credentialed_Scan"
-    cpe = "cpe"
+    partial_cpe = "cpe"
     mac_address = "mac-address"
     nb_name = "netbios-name"
 
@@ -31,7 +31,6 @@ def insert_subparser(subparser):
     mutual_ex_parser = arg_parser.add_mutually_exclusive_group(required=True)
     mutual_ex_parser.add_argument("--filter-value", help="Comma-separated list of values for the filter to match on", type=str)
     mutual_ex_parser.add_argument("--filter-value-file", help="Path to file containing newline-separated list of values for the filter to match on", type=str)
-    arg_parser.add_argument("--full-match", help="Look for full matches (less case sensitivity)", required=False, action="store_true")
     arg_parser.add_argument("--case-sensitive", help="Force alphabet characters in filter values to be case sensitive (default is case insensitive)", required=False, action="store_true")
     arg_parser.add_argument("--negate", help="Negate the filter (i.e. 'only keep hosts which match the filter')", required=False, action="store_true")
     arg_parser.add_argument("--dry-run", help="Do a dry run of the removal, printing some information about any entries that would be removed but not actually outputting the result", required=False, action="store_true")
@@ -43,14 +42,17 @@ def handle(args):
     remove_by = [fp.value for fp in FilterParameters if fp.name == args.remove_by][0]
     negate = args.negate
     case_sensitive = args.case_sensitive
-    full_match = args.full_match
     dry_run = args.dry_run
+
+    if case_sensitive and remove_by in (FilterParameters.ip.value, FilterParameters.was_credential_scanned.value):
+        logger.warn(f"'Case sensitive' flag doesn't have any effect on filter type {remove_by}")
+
     if args.filter_value:
         filter_value_list = args.filter_value.split(",")
     else:
         filter_value_list = open(args.filter_value_file, "r").read().splitlines(keepends=False)
 
-    logger.debug(f"Input filter is: \"Host {remove_by} {'is' if not negate else 'is NOT'} among {filter_value_list}\" (full-match={full_match}, case-sensitive={case_sensitive})")
+    logger.debug(f"Input filter is: \"Host {remove_by} {'is' if not negate else 'is NOT'} among {filter_value_list}\" (case-sensitive={case_sensitive})")
 
     dry_run_res = []
     for host in root.iter("ReportHost"):
@@ -61,10 +63,10 @@ def handle(args):
             logger.debug(f"HostProperty tag named \"{remove_by}\" not found for host named \"{host_name}\", therefore there is not a match.")
             continue
 
-        any_match = check_hostvalue_in_fvlist(remove_by, filter_value_list, host_value, case_sensitive, full_match, negate)
+        any_match = check_hostvalue_in_fvlist(remove_by, filter_value_list, host_value, case_sensitive, negate)
         if any_match and dry_run:
             hostproperties_node = host.find("HostProperties")
-            cpe = get_nessus_hostproperty_by_name(hostproperties_node, FilterParameters.cpe.value, "None Reported")
+            cpe = get_nessus_hostproperty_by_name(hostproperties_node, FilterParameters.partial_cpe.value, "None Reported")
             rdns = get_nessus_hostproperty_by_name(hostproperties_node, "host-rdns", "None Reported")
             msg = f"Host named \"{host_name}\" (primary CPE='{cpe}';  RDNS='{rdns}') would have been removed using provided filter"
             print(msg)
@@ -86,49 +88,39 @@ def handle(args):
             print(result)
         return result
 
-def check_hostvalue_in_fvlist(remove_by, filter_value_list, host_value, case_sensitive, full_match, negate):
+def check_hostvalue_in_fvlist(remove_by, filter_value_list, host_value, case_sensitive, negate):
     for fv in filter_value_list:
-            values_match = determine_match(remove_by, fv, host_value, case_sensitive, full_match)
+            values_match = determine_match(remove_by, fv, host_value, case_sensitive)
             match = values_match if not negate else not values_match
             if match:
                 return True
     return False
 
-def determine_match(remove_by, filter_value, host_value, case_sensitive, full_match):
-    logger.debug(f"Determining if '{host_value}' {'fully matches' if full_match else 'partially matches'} '{filter_value}' {'with case-sensitivity' if case_sensitive else ''} with filter logic for {remove_by}.")
+def determine_match(remove_by, filter_value, host_value, case_sensitive):
+    logger.debug(f"Determining if '{filter_value}' matches '{host_value}' {'with case-sensitivity' if case_sensitive else ''} with filter logic for {remove_by}.")
 
     if remove_by == FilterParameters.ip.value:
-            if case_sensitive or full_match:
-                logger.warn("'Full match' and/or 'case sensitive' flags don't have any effect on IP filters")
             values_match = ipaddress.ip_address(host_value) in ipaddress.ip_network(filter_value, strict=False)
-
-    elif full_match:
-        if remove_by == FilterParameters.was_credential_scanned.value:
-            logger.warn("'Full match' and/or 'case sensitive' flags don't have any effect on bool-valued filters")
-            values_match = strtobool(host_value) == strtobool(filter_value)
-        elif case_sensitive:
-            values_match = host_value == filter_value
+    elif remove_by == FilterParameters.partial_cpe.value:
+        if case_sensitive:
+            values_match = filter_value in host_value
         else:
-            values_match = host_value.lower() == filter_value.lower()
-
+            values_match = filter_value.lower() in host_value.lower() 
+    elif remove_by == FilterParameters.was_credential_scanned.value:
+        values_match = strtobool(host_value) == strtobool(filter_value)
+    elif case_sensitive:    
+        values_match = host_value == filter_value
     else:
-        if remove_by == FilterParameters.was_credential_scanned.value:
-            values_match = strtobool(host_value) == strtobool(filter_value)
-        elif case_sensitive: 
-            values_match = filter_value in host_value 
-        else:
-            values_match = filter_value.lower() in host_value.lower()
+        values_match = host_value.lower() == filter_value.lower()
 
     return values_match
     
 def parse_nessus_host_value(hostnode, remove_by):
-    if remove_by == FilterParameters.cpe.value:
+    if remove_by == FilterParameters.partial_cpe.value:
         host_value = [tag.text for tag in hostnode.find("HostProperties").findall("tag") if tag.get("name").startswith(remove_by)]
-        if len(host_value) == 0:
-            host_value = None
-        host_value = ",".join(host_value)
+        host_value = ",".join(host_value) or None
     else:
-        host_value = get_nessus_hostproperty_by_name(hostnode.find("HostProperties"), remove_by)
+        host_value = get_nessus_hostproperty_by_name(hostnode.find("HostProperties"), remove_by, None)
     
     logger.debug(f"Parsed host value from the scan file: {host_value}")
     return host_value
